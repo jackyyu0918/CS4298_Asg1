d7525fe33f45d5bdceffdd176eac7fd96e9a4021
Jacky YU <jackyyu.github@gmail.com>
2019-10-11 11:00:09 +0800

    Testing commit

---
+++ CS4298_Asg1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CS4298_Asg1
+//
+//  Created by YU Ka Kit on 11/10/2019.
+//  Copyright © 2019 YU Ka Kit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    lanaugesString = 0;
+    
+    if(languageString == 0){
+    
+    }
+
+}
+
---
+++ CS4298_Asg1Tests/CS4298_Asg1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CS4298_Asg1Tests.swift
+//  CS4298_Asg1Tests
+//
+//  Created by YU Ka Kit on 11/10/2019.
+//  Copyright © 2019 YU Ka Kit. All rights reserved.
+//
+
+import XCTest
+@testable import CS4298_Asg1
+
+class CS4298_Asg1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
