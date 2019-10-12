ae41759e3534fbaac22fdb4ea142418be140a9fb
Virgil <Virgil@gmail.com>
2019-10-12 18:00:24 +0800

    Vrigil Change

---
+++ CS4298_Asg1/Controller/MyNexCalendar.swift
@@ -0,0 +1,22 @@
+//
+//  MyNexCalendar.swift
+//  CS4298_Asg1
+//
+//  Created by CHAN Yat Chau on 12/10/2019.
+//  Copyright © 2019 YU Ka Kit. All rights reserved.
+//
+
+import Foundation
+
+
+import UIKit
+
+class MyNexCalendar: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Running")
+    }
+    
+}
+
