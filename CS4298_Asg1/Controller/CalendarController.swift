c66b14ab860b402c64833012cd0ce4f3416de929
Virgil <chanyatchau@gmail.com>
2019-10-21 20:16:45 +0800

    Update Change Colour

--- CS4298_Asg1/Controller/CalendarController.swift
+++ CS4298_Asg1/Controller/CalendarController.swift
@@ -162,7 +162,7 @@
                     textLabel.backgroundColor = UIColor.darkGray
                 }else if( indexPath.row - dayToStart < dayToEnd){
                     textLabel.text = "\(indexPath.row - dayToStart + 1)"
-                    textLabel.backgroundColor = UIColor.lightGray
+                    textLabel.backgroundColor = UIColor(hexFromString: "51B3FF")
                 }
                 else{
                     if(isLeapYear && calendarMonth == 12 && indexPath.row == 41){
@@ -219,5 +219,27 @@
             if motion == .motionShake{
                 currentDate(self)
             }
+        }
+     }
+     
+     extension UIColor {
+        convenience init(hexFromString:String, alpha:CGFloat = 1.0) {
+            var cString:String = hexFromString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+            var rgbValue:UInt32 = 10066329 //color #999999 if string has wrong format
+            
+            if (cString.hasPrefix("#")) {
+                cString.remove(at: cString.startIndex)
+            }
+            
+            if ((cString.count) == 6) {
+                Scanner(string: cString).scanHexInt32(&rgbValue)
+            }
+            
+            self.init(
+                red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                alpha: alpha
+            )
         }
      }
