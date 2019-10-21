8db1a1cff5c4776e1b89f68da78d825435218ea4
d <d>
2019-10-21 13:47:38 +0800

    andy:

--- CS4298_Asg1/Controller/Infor_Benefit.swift
+++ CS4298_Asg1/Controller/Infor_Benefit.swift
@@ -9,17 +9,38 @@ import UIKit
 
 class Infor_Benefit: UIViewController {
     
-    @IBOutlet weak var First_Benefit: UILabel!
+    
+    @IBOutlet weak var FirstBenefit_label: UILabel!
+    
+    @IBOutlet weak var SecondBenefit_label: UILabel!
+    
+    @IBOutlet weak var ThirdBenefit_label: UILabel!
+    
+    @IBOutlet weak var FourthBenefit_label: UILabel!
+    
+    @IBOutlet weak var FifthBenefit_label: UILabel!
+    
+    
+    @IBOutlet weak var Homepage_button: UIButton!
+    
     
     override func viewDidLoad() {
         
         
         if(Language_Control.language == 0){
-            First_Benefit.text = Language_Control.English_String
+
+            
+            
             
         }
         else if(Language_Control.language == 1){
-            First_Benefit.text = Language_Control.Chinese_String
+            
+            FirstBenefit_label.text = InformationLanguage.First_benfit
+            SecondBenefit_label.text = InformationLanguage.Second_benfit
+            ThirdBenefit_label.text = InformationLanguage.Third_benefit
+            FourthBenefit_label.text = InformationLanguage.Fourth_benefit
+            FifthBenefit_label.text = InformationLanguage.Fifth_benefit
+            Homepage_button.setTitle(InformationLanguage.homepage, for: .normal)
             
         }
         
---
+++ CS4298_Asg1/Model/InformatonLanguage.swift
@@ -0,0 +1,34 @@
+//
+//  InformatonLanguage.swift
+//  CS4298_Asg1
+//
+//  Created by CHOW Ho Hin on 21/10/2019.
+//  Copyright © 2019 YU Ka Kit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct InformationLanguage {
+    
+    static var homepage = "回到主頁"
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Benefit section
+     static var First_benfit = "✓可以用於任何年份"
+     static var Second_benfit = "✓簡單及統一"
+     static var Third_benefit = "✓容易記憶"
+     static var Fourth_benefit = "✓有經濟效益及有效"
+     static var Fifth_benefit = "✓實際及有益"
+    
+    
+}
